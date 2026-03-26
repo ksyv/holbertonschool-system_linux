6433f51ab963376a003febc46977354c36674a97
ksyv <ksyviais@gmail.com>
2026-03-26 11:18:11 +0100

    betty compliant 3

--- ls/hls.h
+++ ls/hls.h
@@ -16,7 +16,8 @@ typedef struct opt_s
 	int one;
 } opt_t;
 
-int process_arg(const char *arg, const char *prog_name, int multiple, opt_t *opt);
+int process_arg(const char *arg, const char *prog_name, int multiple,
+opt_t *opt);
 int parse_options(int argc, char **argv, opt_t *opt);
 
 #endif /* HLS_H */
